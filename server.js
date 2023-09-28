f4a50f01e1e545a6f0e3d6e06a1eee816e737756
Harshul Jain <harshulgangwal02@gmail.com>
2023-09-28 17:32:44 +0530

    Changes

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const db = require('./db');
 const roomsRoute = require('./routers/roomsRoute');
 const usersRoute = require('./routers/usersRoute');
 const bookingsRoute = require('./routers/bookingsRoute');
-
-const corsOptions = {
-    origin: 'http://localhost:3000', // Replace with the origin of your frontend application
-    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/rooms', roomsRoute);
